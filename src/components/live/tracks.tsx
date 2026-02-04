cc3fa3b1acdec47d48741057615ad63093caaf8c
j1munoz <jimmymunoz397@gmail.com>
2026-02-04 11:25:56 -0800

    Updated tracks

--- src/components/live/tracks.tsx
+++ src/components/live/tracks.tsx
@@ -1,6 +1,11 @@
+"use client";
+
 import Cake from "@/public/landing/track1.svg";
 import Bear from "@/public/landing/track2.svg";
 import StrawberryCake from "@/public/landing/track3.svg";
+import Framer from "@/public/landing/track4.svg";
+import { easeOut, motion } from "motion/react";
+
 import Image from "next/image";
 
 const tracks = [
@@ -19,7 +24,23 @@ const tracks = [
     title: "Sustainable Sweets",
     customStyles: "text-landing-brown-50",
   },
+  {
+    image: Framer,
+    title: "Framaccino",
+    customStyles: "text-white",
+  },
 ];
+
+const trackAnimation = (index: number) => ({
+  initial: { opacity: 0, y: 30 },
+  whileInView: { y: 0, opacity: 1 },
+  transition: { delay: 0.2 * index, duration: 0.5 },
+  viewport: { once: true },
+});
+
+const hoverAnimation = () => ({
+  whileHover: { scale: 1.08, transition: { duration: 0.3, ease: easeOut } },
+});
 
 const Tracks = () => {
   return (
@@ -27,19 +48,21 @@ const Tracks = () => {
       <p className="font-inter text-landing-brown-300 py-8 text-center text-4xl font-bold md:text-7xl">
         TRACKS
       </p>
-      <div className="flex flex-col gap-12 py-12 md:flex-row">
+      <div className="grid grid-cols-1 gap-12 py-12 lg:grid-cols-3">
         {tracks.map(({ image, title, customStyles }, index) => (
-          <div
-            key={index}
-            className="relative z-20 flex items-center justify-center"
-          >
-            <p
-              className={`font-inter absolute top-28 z-10 w-48 text-center text-2xl font-semibold break-words ${customStyles}`}
+          <motion.div key={index} {...trackAnimation(index)}>
+            <motion.div
+              {...hoverAnimation()}
+              className="relative z-20 flex items-center justify-center"
             >
-              {title}
-            </p>
-            <Image key={index} src={image} alt={title} />
-          </div>
+              <p
+                className={`font-inter absolute top-28 z-10 w-48 text-center text-2xl font-semibold break-words ${customStyles}`}
+              >
+                {title}
+              </p>
+              <Image key={index} src={image} alt={title} />
+            </motion.div>
+          </motion.div>
         ))}
       </div>
     </div>
