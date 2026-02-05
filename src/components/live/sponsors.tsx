aa82d71ad0ce60f9a1bb3f30b663e1139e0f4a36
Sweden <sweden.agunenye1@gmail.com>
2026-02-04 22:12:32 -0800

    added sponsors

--- src/components/live/sponsors.tsx
+++ src/components/live/sponsors.tsx
@@ -7,13 +7,13 @@ const Sponsors = () => {
   return (
     <div className="flex flex-col items-center justify-center gap-8 pt-12">
       <Title>SPONSORS</Title>
-      <div className="max-w-8xl grid w-5/6 grid-cols-1 items-center justify-center gap-8 p-6 sm:grid-cols-3 sm:gap-1">
+      <div className="max-w-8xl flex w-5/6 flex-wrap items-center justify-center gap-8 p-6 sm:gap-1">
         {sponsors.map(({ name, image, translateY, link }, index) => (
           <Link
             key={index}
             href={link}
             target="_blank"
-            className={`flex items-center justify-center transition-transform hover:scale-110 ${translateY ? "-translate-y-[6%]" : ""}`}
+            className={`flex w-1/4 items-center justify-center transition-transform hover:scale-110 ${translateY ? "-translate-y-[6%]" : ""}`}
           >
             <Image src={image} alt={name} className="w-2/3 sm:w-3/4" />
           </Link>
--- src/data/sponsors/sponsors.ts
+++ src/data/sponsors/sponsors.ts
@@ -5,6 +5,8 @@ import Sponsor3 from "@/public/landing/sponsor3.svg";
 import Sponsor4 from "@/public/landing/sponsor4.svg";
 import Sponsor5 from "@/public/landing/sponsor5.svg";
 import Sponsor6 from "@/public/landing/sponsor6.svg";
+import Sponsor7 from "@/public/landing/sponsor7.svg";
+import Sponsor8 from "@/public/landing/sponsor8.svg";
 
 export interface Sponsor {
   image: StaticImageData;
@@ -16,38 +18,50 @@ export interface Sponsor {
 export const sponsors: Sponsor[] = [
   {
     image: Sponsor1,
-    name: "Sponsor 1",
+    name: "Pure Buttons",
     translateY: false,
     link: "https://www.purebuttons.com/",
   },
   {
     image: Sponsor2,
-    name: "Sponsor 2",
+    name: "Creat'R Lab",
     translateY: true,
-    link: "https://www.notion.com/",
+    link: "https://library.ucr.edu/research-support/making-and-innovation/creatr-lab-makerspace",
   },
   {
     image: Sponsor3,
-    name: "Sponsor 3",
+    name: "Progressive Graphics",
     translateY: false,
-    link: "/",
+    link: "https://progressive-graphics.com/Progressive_Graphics/shop/home",
   },
   {
     image: Sponsor4,
-    name: "Sponsor 4",
+    name: "Notion",
     translateY: false,
-    link: "/",
+    link: "https://www.notion.com/",
   },
   {
     image: Sponsor5,
-    name: "Sponsor 5",
+    name: "Monster Energy",
     translateY: true,
-    link: "/",
+    link: "https://www.monsterenergy.com/en-us/",
   },
   {
     image: Sponsor6,
-    name: "Sponsor 6",
+    name: "Balsamiq",
     translateY: false,
-    link: "/",
+    link: "https://balsamiq.com/",
+  },
+  {
+    image: Sponsor7,
+    name: "Framer",
+    translateY: false,
+    link: "https://www.framer.com/",
+  },
+  {
+    image: Sponsor8,
+    name: "Azure RP",
+    translateY: false,
+    link: "https://www.axure.com/",
   },
 ];
