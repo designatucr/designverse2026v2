a2b1bbb0c0f9319d2d0f19bbbc45fed2557a56f2
Charleen Chen <charleenc485@gmail.com>
2026-02-05 14:54:58 -0800

    added table to footer

--- src/components/live/footer.tsx
+++ src/components/live/footer.tsx
@@ -5,70 +5,78 @@ import Image from "next/image";
 import { FaDiscord, FaLinkedin } from "react-icons/fa";
 import { RiInstagramFill } from "react-icons/ri";
 import { MdEmail } from "react-icons/md";
+import table from "@/public/landing/table.svg";
 
 const Footer = () => {
   return (
-    <footer className="bg-landing-brown-300 flex flex-col justify-between gap-2 p-6 md:p-16">
-      <div className="flex flex-row justify-between">
-        <div>
-          <Image
-            className="w-1/3 pb-5 md:pb-10 md:pl-5"
-            src={LogoIcon}
-            alt="DV Logo in footer"
-          />
-          <div className="flex flex-row md:pl-5">
+    <div className="relative">
+      <Image
+        className="absolute bottom-[90%] left-[30%] w-[40%] hidden md:block"
+        src={table}
+        alt="table and 2 chairs"
+      />
+      <div className="bg-landing-brown-300 flex flex-col justify-between gap-2 p-6 md:p-16">
+        <div className="flex flex-row justify-between">
+          <div>
+            <Image
+              className="w-1/3 pb-5 md:pb-10 md:pl-5"
+              src={LogoIcon}
+              alt="DV Logo in footer"
+            />
+            <div className="flex flex-row md:pl-5">
+              <Link
+                className="pr-3"
+                href="mailto:designverseucr@gmail.com"
+                target="_blank"
+                aria-label="Email"
+              >
+                <MdEmail className="text-2xl text-white transition-transform hover:scale-110 md:text-5xl" />
+              </Link>
+              <Link
+                className="pr-3"
+                href="https://www.linkedin.com/company/designverseucr/"
+                target="_blank"
+                aria-label="Linkedin"
+              >
+                <FaLinkedin className="text-2xl text-white transition-transform hover:scale-110 md:text-5xl" />
+              </Link>
+              <Link
+                className="pr-3"
+                href="https://www.instagram.com/designverseucr"
+                target="_blank"
+                aria-label="Instagram"
+              >
+                <RiInstagramFill className="text-2xl text-white transition-transform hover:scale-110 md:text-5xl" />
+              </Link>
+              <Link
+                href="https://discord.com/invite/MmSvY3tu"
+                target="_blank"
+                aria-label="Discord"
+              >
+                <FaDiscord className="text-2xl text-white transition-transform hover:scale-110 md:text-5xl" />
+              </Link>
+            </div>
+            <p className="font-inter py-5 text-left text-xs text-white md:pl-5 md:text-2xl">
+              Made with &lt;3 by the 2026 DesignVerse team
+            </p>
+          </div>
+          <div className="flex flex-col text-right md:pr-5">
+            <p className="font-inter text-sm font-bold text-white md:text-2xl">
+              Previous Sites
+            </p>
             <Link
-              className="pr-3"
-              href="mailto:designverseucr@gmail.com"
+              className="font-inter py-3 text-sm text-white hover:cursor-pointer hover:underline md:text-2xl"
+              href="https://designverse2025.vercel.app/"
               target="_blank"
-              aria-label="Email"
+              rel="noreferrer noopener"
+              aria-label="2025 Website"
             >
-              <MdEmail className="text-2xl text-white transition-transform hover:scale-110 md:text-5xl" />
-            </Link>
-            <Link
-              className="pr-3"
-              href="https://www.linkedin.com/company/designverseucr/"
-              target="_blank"
-              aria-label="Linkedin"
-            >
-              <FaLinkedin className="text-2xl text-white transition-transform hover:scale-110 md:text-5xl" />
-            </Link>
-            <Link
-              className="pr-3"
-              href="https://www.instagram.com/designverseucr"
-              target="_blank"
-              aria-label="Instagram"
-            >
-              <RiInstagramFill className="text-2xl text-white transition-transform hover:scale-110 md:text-5xl" />
-            </Link>
-            <Link
-              href="https://discord.com/invite/MmSvY3tu"
-              target="_blank"
-              aria-label="Discord"
-            >
-              <FaDiscord className="text-2xl text-white transition-transform hover:scale-110 md:text-5xl" />
+              2025
             </Link>
           </div>
-          <p className="font-inter py-5 text-left text-xs text-white md:pl-5 md:text-2xl">
-            Made with &lt;3 by the 2026 DesignVerse team
-          </p>
-        </div>
-        <div className="flex flex-col text-right md:pr-5">
-          <p className="font-inter text-sm font-bold text-white md:text-2xl">
-            Previous Sites
-          </p>
-          <Link
-            className="font-inter py-3 text-sm text-white hover:cursor-pointer hover:underline md:text-2xl"
-            href="https://designverse2025.vercel.app/"
-            target="_blank"
-            rel="noreferrer noopener"
-            aria-label="2025 Website"
-          >
-            2025
-          </Link>
         </div>
       </div>
-    </footer>
+    </div>
   );
 };
 
